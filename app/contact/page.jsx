1e134c387492c1c13e25a60d1ad736499683279e
Romain Hernandez <romsher.dev@gmail.com>
2024-07-30 16:34:02 +0200

    chore: add metadate on contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,10 @@
 import Contact from "../src/components/main/contact/Contact";
 
+export const metadata = {
+  title: "Roms - Contact",
+  description: "Contactez-moi pour toute demande. Je serai ravi de m'entretenir avec vous pour discuter de celle-ci.",
+};
+
 export default function ContactPage() {
   return (
     <Contact></Contact>
