f3e7d3e35603ea8608df79b8998b76d655e07530
Romain Hernandez <romsher.dev@gmail.com>
2024-08-10 10:33:59 +0200

    chore: metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import Nav from "./src/components/header/Nav";
 const inter = Roboto({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
-  title: "Roms - Portfolio",
+  title: "Roms - Développeur web et web mobile",
   description: "Bienvenue sur mon portfolio. Je suis développeur web et web mobile, je vous invite à découvrir mes projets et mes compétences.",
 };
 
